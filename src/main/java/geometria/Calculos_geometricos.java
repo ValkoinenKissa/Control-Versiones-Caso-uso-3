7ae7efe7ef9650ad1be02d66c9c06235b23651b2
Alberto Bollo <albertobollo7@protonmail.com>
2024-01-18 22:21:53 +0100

    Añadidas funcionalidades calculo area cilindro

--- src/main/java/geometria/Calculos_geometricos.java
+++ src/main/java/geometria/Calculos_geometricos.java
@@ -12,13 +12,24 @@ public class Calculos_geometricos {
 
 	public static void Area_triangulo() {
 		// Calculo area del triangulo
-		
-				System.out.println("Ingrese la longitud del lado 1 del triángulo: ");
-				int lado1 = scanner.nextInt();
-				System.out.println("Ingrese la longitud del lado 2 del triángulo: ");
-				int lado2 = scanner.nextInt();
-				double area = calcularAreaTriangulo(lado1, lado2);
-				System.out.println("El área del triángulo es: " + area);
+
+		System.out.println("Ingrese la longitud del lado 1 del triángulo: ");
+		int lado1 = scanner.nextInt();
+		System.out.println("Ingrese la longitud del lado 2 del triángulo: ");
+		int lado2 = scanner.nextInt();
+		double area = calcularAreaTriangulo(lado1, lado2);
+		System.out.println("El área del triángulo es: " + area);
+
+		System.out.println("Introduce el radio del cilindro: ");
+		double radio = scanner.nextDouble();
+		System.out.println("Introduce la altura del cilindro: ");
+		double altura = scanner.nextDouble();
+
+		double resultBase = calcularAreaBase(radio);
+		double areaLateral = calcularAreaLateral(radio, altura);
+		double areaTotal = areaLateral + (2 * resultBase);
+
+		imprimeResult(areaLateral, areaTotal);
 				
 	}
 	
@@ -47,5 +58,23 @@ public class Calculos_geometricos {
     public static double calcularPerimetroCircunferencia(double radio) {
         return 2 * Math.PI * radio;
     }
+    
+    //Metodos para calcular area del cilindro
+    
+    public static double calcularAreaBase(double radio) {
+        double resultadoAreaBase = Math.PI * Math.pow(radio, 2);
+        return resultadoAreaBase;
+    }
 
+    public static double calcularAreaLateral(double radio, double altura) {
+        double areaLateralSupercifie = 2 * Math.PI * radio * altura;
+        return areaLateralSupercifie;
+    }
+
+    public static void imprimeResult(double areaLateral, double areaTotal) {
+        System.out.println("El area lateral de la superficie del cilindro es: " + areaLateral);
+        System.out.println("El area total de la superficie del cilindro: " + areaTotal);
+    }
+    
+    
 }
